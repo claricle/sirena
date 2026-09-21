9fe9717a496f943e3bf5990a1c2b40055ff242c6
HassanAkbar <hassanakbar2@gmail.com>
2026-09-21 19:50:20 +0500

    move changelog run_script helper to spec/support

--- spec/scripts/check_changelog_spec.rb
+++ spec/scripts/check_changelog_spec.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "open3"
-require "rbconfig"
-require "tmpdir"
 require_relative "../../scripts/check_changelog"
 
 RSpec.describe Sirena::ChangelogCheck do
@@ -66,16 +63,7 @@ RSpec.describe Sirena::ChangelogCheck do
   end
 
   describe "command line" do
-    def run_script(*args, changelog:)
-      Dir.mktmpdir do |dir|
-        path = File.join(dir, "CHANGELOG.md")
-        File.write(path, changelog)
-        version = File.join(dir, "version.rb")
-        File.write(version, "VERSION = '0.1.9'\n")
-        script = File.expand_path("../../scripts/check_changelog.rb", __dir__)
-        Open3.capture3(RbConfig.ruby, script, args.first, path, version)
-      end
-    end
+    include ChangelogScriptHelper
 
     it "exits 0 when releasable" do
       _out, _err, status = run_script("0.2.0", changelog: good)
